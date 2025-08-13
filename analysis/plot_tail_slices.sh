924181b81201b3fb635796a947445a7d75eaace4
Sage Stanish <sagestanish@posteo.net>
2025-08-13 10:36:44 +0100

    cleaned up analysis dir and added figs summary

--- analysis/plot_tail_slices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..819}
-do
-  ./format_field ../data/july_scans/s02/output_$i.nc 12
-  ./format_rate ../data/july_scans/s02/rate_$i.nc
-  gnuplot plot_tail.gnu > ../figs/july/s02/slice_$i.svg
-done

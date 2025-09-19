87bf628640ed9a8d6fdee4b9a8f50b291df34c98
Sage Stanish <sagestanish@posteo.net>
2025-09-19 16:30:28 +0100

    rbf centers passes test. Shared continue statments in fortran are aweful!

--- src/TA16.f90
+++ src/TA16.f90
@@ -1,70 +1,74 @@
-module TA16
+  module TA16
 
-use iso_fortran_env, only: real64
+  use iso_fortran_env, only: real64
 
-implicit none
+  implicit none
 
-integer, parameter :: dp=real64
+  integer, parameter :: dp=real64
 
-real(dp) :: A(23328)
+  !real(dp) :: A(23328)
 
-contains
+  contains
 
-subroutine calculate_rbf_centers(x,y,z,st,rho,zsp,zcp,rhbr)
+  subroutine calculate_rbf_centers(x,y,z,st,rho,zsp,zcp,rhbr)
 
-  real(dp), intent(out), dimension(1296) :: x,y,z,st,rho,zsp,zcp,rhbr
+    real(dp), intent(out), dimension(1296) :: x,y,z,st,rho,zsp,zcp,rhbr
 
-  integer :: klat, nlat, nlon
+    integer :: klat, nlat, nlon
 
-  real(dp) :: rlow_grid, rhigh_grid
-  real(dp) :: dlat_deg, dlon_deg, xcolat
-  real(dp) :: Radius
-  real(dp) :: theta_n, theta_s, ctn, cts, stn, sts, sp, cp
-  real(dp) :: zp, yp, xp, xlond, xlon, T, stt, rm, R1, R0
-  real(dp) :: psin, psis, last_radius, f1, es, en, ds, dn, ctt, cn, b2, b0
+    real(dp) :: rlow_grid, rhigh_grid
+    real(dp) :: dlat_deg, dlon_deg, xcolat
+    real(dp) :: Radius
+    real(dp) :: theta_n, theta_s, ctn, cts, stn, sts, sp, cp
+    real(dp) :: zp, yp, xp, xlond, xlon, T, stt, rm, R1, R0
+    real(dp) :: psin, psis, last_radius, f1, ds, dn, ctt, cn, b2, b0
 
-  real(dp), parameter :: rh=8.0,alpha=3.0,pd_tr=0.5
-  real(dp), parameter :: psi=0.0,pm=0.0,Bzimf_tr=0.0
-  real(dp), parameter :: pi=4.0_dp*atan(1.0_dp)
+    real(dp), parameter :: rh=8.0_dp,alpha=3.0_dp,pd_tr=0.5_dp
+    real(dp), parameter :: psi=0.0_dp,pm=0.0_dp,Bzimf_tr=0.0_dp
+  real(dp), parameter :: pi=3.14159265359_dp !4.0_dp*atan(1.0_dp)
 
-  real(dp), dimension(22) :: An(0:21) = [12.544,-0.194,0.305,0.0573,2.178,0.0571,-0.999,16.473,0.00152,0.382,0.0431,-0.00763,-0.210,0.0405,-4.430,-0.636,-2.600,0.832,-5.328,1.103,-0.907,1.45]
+  real(dp), dimension(22) :: An(0:21) = [12.544_dp,-0.194_dp,0.305_dp,0.0573_dp,2.178_dp,0.0571_dp,-0.999_dp,16.473_dp,0.00152_dp,0.382_dp,0.0431_dp,-0.00763_dp,-0.210_dp,0.0405_dp,-4.430_dp,-0.636_dp,-2.600_dp,0.832_dp,-5.328_dp,1.103_dp,-0.907_dp,1.45_dp]
 
   integer :: i,j,k,l
 
   klat = 8
   nlat = klat + 1
 
-  rlow_grid = 3.3
-  rhigh_grid = 16.0
+  rlow_grid = 3.3_dp
+  rhigh_grid = 16.0_dp
 
-  dlat_deg = 90.0/(nlat-0.5)
+  dlat_deg = 90.0_dp/(nlat-0.5_dp)
 
   l = 0
 
   Radius = rlow_grid
 
-  An(10:14) = 0.0 ! Simplified version of Lin et al model
+  An(10) = 0.0_dp ! Simplified version of Lin et al model
+  An(11) = 0.0_dp
+  An(13) = 0.0_dp
+  An(14) = 0.0_dp
   
-  do j = 1,100
+while_loop : do j = 1,100 ! This is a while loop for R<=Rhigh_grid. It is written this way to include the very last iteration once the local loop is broken
 
-    do i = 1,nlat
+outer_loop : do i = 1,nlat
+      print *, i
 
       nlon = 4*(i-1)
 
       if (i /= 1) then
-        dlon_deg = 360.0/nlon
+        dlon_deg = 360.0_dp/nlon
       else
         nlon = 1
-        dlon_deg = 0.0
+        dlon_deg = 0.0_dp
       end if
 
-      do k = 1,nlon
+inner_loop : do k = 1,nlon
 
         ! Coordinate transform
         xlond = (k-1)*dlon_deg
 
-        xcolat = (2.0*pi/360.0) * dlat_deg * (i-1)
-        xlon = (2.0*pi/360.0) * dlon_deg * (k-1)
+        xcolat = (2.0_dp*pi/360.0_dp) * dlat_deg * (i-1.0_dp)
+        xlon = (2.0_dp*pi/360.0_dp) * dlon_deg * (k-1.0_dp)
 
         xp = Radius*sin(xcolat)*cos(xlon)
         yp = Radius*sin(xcolat)*sin(xlon)
@@ -88,11 +92,11 @@ subroutine calculate_rbf_centers(x,y,z,st,rho,zsp,zcp,rhbr)
         if (sqrt(yp**2 + zp**2) > 1.d-5) then
           sp = zp/R1
           cp = yp/R1
-        else if ( xp > 0.0) then
-          sp = 0.0
-          cp = 1.0
+        else if ( xp > 0.0_dp) then
+          sp = 0.0_dp
+          cp = 1.0_dp
         else
-          Radius = 1000.0
+          Radius = 1000.0_dp
           return
         end if
 
@@ -107,15 +111,19 @@ subroutine calculate_rbf_centers(x,y,z,st,rho,zsp,zcp,rhbr)
         b0 = An(6) + An(7)*(exp(An(8)*Bzimf_tr)-1.0)/(exp(An(9)*Bzimf_tr)+1.0)
         b2 = An(11)+An(12)*psi
 
-        f1 = ( sqrt(0.50 * (1.0+ctt)) + An(5)*2.0*stt*ctt*(1.0-exp(-T)))**(b0+An(10)*cp+b2*sp+An(13)*sp**2)
+        f1 = ( sqrt(0.5_dp * (1.0_dp+ctt)) + An(5)*2.0_dp*stt*ctt*(1.0_dp-exp(-T)))**(b0+An(10)*cp+b2*sp+An(13)*sp**2)
 
-        R0 = An(0)*(pd_tr+pm)**An(1) * ( 1.0 + An(2)*(exp(An(3)*Bzimf_tr)-1.0)/(exp(An(4)*Bzimf_tr)+1.0) )
-        Rm = R0*f1 + cn*exp(dn*psin**en) + cn*exp(ds*psis**es)
+        R0 = An(0)*(pd_tr+pm)**An(1) * ( 1.0_dp + An(2)*(exp(An(3)*Bzimf_tr)-1.0_dp)/(exp(An(4)*Bzimf_tr)+1.0_dp) )
+        Rm = R0*f1 + cn*exp(dn*psin**An(21)) + cn*exp(ds*psis**An(21))
+        print *, Rm
 
-        if (Radius > Rm) exit
+        if (Radius .GT. Rm) then
+          print *, "fuck"
+          cycle
+          !exit inner_loop
+        end if 
 
         l = l+1
-
         x(l) = xp
         y(l) = yp
         z(l) = zp
@@ -123,34 +131,34 @@ subroutine calculate_rbf_centers(x,y,z,st,rho,zsp,zcp,rhbr)
         rho(l) = Radius*st(l)
         zsp(l) = z(l)*sin(xlon)
         zcp(l) = z(l)*cos(xlon)
-        rhbr(l) = rh/Radius * (1.0 - (1.0 + (Radius/rh)**alpha)**(1.0/alpha))
+        rhbr(l) = rh/Radius * (1.0_dp - (1.0_dp + (Radius/rh)**alpha)**(1.0_dp/alpha))
 
-      end do
+      end do inner_loop
+    end do outer_loop
 
-      last_radius = Radius
-      Radius = Radius*(nlat - 0.5 + pi/4.0)/(nlat - 0.5 - pi/4.0)
+    print *, xcolat
+    Radius = Radius*(nlat - 0.5_dp + pi/4.0_dp)/(nlat - 0.5_dp - pi/4.0_dp)
 
-      if (Radius > rhigh_grid) return
+    if (Radius .GT. rhigh_grid) exit while_loop
 
-    end do
-  end do
+  end do while_loop
 
 end subroutine calculate_rbf_centers
 
 subroutine calculate_ta16_field
 end subroutine calculate_ta16_field
 
-subroutine read_ta16_parameters(filename)
+!subroutine read_ta16_parameters(filename)
 
-  character(*), intent(in) :: filename
-  integer :: ta_file, i
+! character(*), intent(in) :: filename
+! integer :: ta_file, i
+!
+! open(newunit=ta_file,file=filename,action="read")
+! do i=1,size(A)
+!   read(ta_file,"(G15.6)") A(i)
+! end do
+! close(ta_file)
 
-  open(newunit=ta_file,file=filename,action="read")
-  do i=1,size(A)
-    read(ta_file,"(G15.6)") A(i)
-  end do
-  close(ta_file)
-
-end subroutine read_ta16_parameters
+!end subroutine read_ta16_parameters
 
 end module TA16
--- src/tests/ta16_tests.f90
+++ src/tests/ta16_tests.f90
@@ -5,43 +5,40 @@ program main
   implicit none
 
   integer, parameter :: dp=real64
-  real(dp) :: out_error
 
-  call test_rbf_centers(out_error)
-  print *, "rbf centers subroutine, abs error;"
-  print *, "     ", out_error
+  call test_rbf_centers
 
 contains
 
-  subroutine test_rbf_centers(errors)
+  subroutine test_rbf_centers
     ! Tests new rbf_centers subroutine against the original 
     ! TA16 calculation. New variables have an n appended
     ! in front. 
-    !
-    ! Returns number of mismatched values
-
+    
     use Original_TA16
     use TA16, only : calculate_rbf_centers
 
     real(dp), dimension(1296) :: nx,ny,nz,nst,nrho,nzsp,nzcp,nrhbr
     real(dp) :: hold_parmod(10), hbx,hby,hbz
-    real(dp), intent(out) :: errors
     integer :: i
 
     hold_parmod = 1.0_dp
     call calculate_rbf_centers(nx,ny,nz,nst,nrho,nzsp,nzcp,nrhbr)
     call RBF_MODEL_2016(0,hold_parmod, 1.0_dp, 1.0_dp, 1.0_dp, 1.0_dp, hbx, hby, hbz)
 
-    do i = 1,size(nx)
-      print *, nx(i), " ", XX(i)
+    do i = 1,size(rho)
+      print *, nrho(i), " ", RHO(i)
     end do
 
-    errors = 0.0
-    errors = errors + sum(abs(nx-XX))
-    !errors = errors + sum(abs(ny-YY))
-    !errors = errors + sum(abs(nz-ZZ))
-    !errors = errors + sum(abs(nst-ST))
-    !errors = errors + sum(abs(nrho-RHO))
+    print *, "rbf centers subroutine errors;"
+    print *, "  x -> ", sum(abs(nx-XX))
+    print *, "  y -> ", sum(abs(ny-YY))
+    print *, "  z -> ", sum(abs(nz-ZZ))
+    print *, "  st -> ", sum(abs(nst-ST))
+    print *, "  rho -> ", sum(abs(nrho-RHO))
+    print *, "  zsp -> ", sum(abs(nzsp-ZSP))
+    print *, "  zcp -> ", sum(abs(nzcp-ZCP))
+    print *, "  rhbr -> ", sum(abs(nrhbr-RHBR))
 
   end subroutine test_rbf_centers
 

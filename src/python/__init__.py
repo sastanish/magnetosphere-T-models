c487196c1292269a1503d2d0af2e5729e86936e4
Sage Stanish <sagestanish@posteo.net>
2026-02-13 11:59:24 +0000

    pip install . works now

---
+++ src/python/__init__.py
@@ -0,0 +1,1 @@
+from .magnetoTmodels import ta16, find_magnetosphere_library
--- src/python/magnetoTmodels.py
+++ src/python/magnetoTmodels.py
@@ -1,8 +1,36 @@
 import os
+import sys
 from numpy import ctypeslib, zeros
 from ctypes import c_int, byref, util, CDLL
 
+def find_magnetosphere_library():
+  lib_name = 'magnetoTpython'
+  install_path = os.path.dirname(os.path.realpath(__file__))
+
+  print(install_path)
+
+  if os.name == 'posix': # Linux or macOS
+    if os.uname().sysname == 'Darwin': # macOS
+      lib_path = install_path + f'/lib{lib_name}.dylib'
+    else: # Linux
+      lib_path = install_path + f'/lib{lib_name}.so'
+  elif os.name == 'nt': # Windows
+      lib_path = install_path + f'/{lib_name}.dll'
+  else:
+    raise RuntimeError("Unsupported operating system")
+
+  if not lib_path:
+    raise FileNotFoundError(f"Could not find shared library '{lib_name}'. Make sure it's installed.")
+
+  try:
+    fortran_lib = CDLL(lib_path)
+  except OSError as e:
+    print(f"Error loading library: {e}")
+    print(f"Attempted path: {lib_path}")
+  return fortran_lib
+
 def ta16(date,data,x,y,z):
+
   nx = len(x)
   ny = len(y)
   nz = len(z)
@@ -10,7 +38,7 @@ def ta16(date,data,x,y,z):
   by = zeros( (nx,ny,nz) )
   bz = zeros( (nx,ny,nz) )
 
-  ta16 = fortran_lib.ta16
+  api = find_magnetosphere_library()
 
   api.ta16( byref(ctypeslib.as_ctypes(date)),
             byref(ctypeslib.as_ctypes(data)),
@@ -25,28 +53,3 @@ def ta16(date,data,x,y,z):
             byref(c_int(nz))
             )
   return (bx, by, bz)
-
-if __name__=="__main__":
-
-  lib_name = 'magnetoTpython'
-  install_path = os.path.dirname(os.path.realpath(__file__))
-  print(install_path)
-
-  if os.name == 'posix': # Linux or macOS
-    if os.uname().sysname == 'Darwin': # macOS
-      lib_path = install_path + f'../../lib/lib{lib_name}.dylib'
-    else: # Linux
-      lib_path = install_path + f'../../lib/lib{lib_name}.so'
-  elif os.name == 'nt': # Windows
-      lib_path = install_path + f'../../lib/{lib_name}.dll')
-  else:
-    raise RuntimeError("Unsupported operating system")
-
-  if not lib_path:
-    raise FileNotFoundError(f"Could not find shared library '{lib_name}'. Make sure it's installed.")
-
-  try:
-    fortran_lib = CDLL(lib_path)
-  except OSError as e:
-    print(f"Error loading library: {e}")
-    print(f"Attempted path: {lib_path}")
